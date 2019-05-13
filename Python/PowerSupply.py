dd9eaa8ef66a8cb8594cc6e731d114748c375340
Quanxin Mei <meiquanxin@gmail.com>
2019-05-14 03:29:32 +0800

    first commit

---
+++ Python/PowerSupply.py
@@ -0,0 +1,14 @@
+import serial
+
+if __name__ == "__main__":
+    ps = serial.Serial()
+    ps.baudrate = 9600
+    ps.port = "COM3"
+    ps.open()
+    
+    if(ps.open):
+        ps.write(b"SYSTEM:INTERFACE RS232")
+        ps.write(b"SYSTEM:REMOTE")
+        ps.write(b"VOLTAGE 2.0")
+        ps.write(b"CURRENT 2.1")
+        ps.write(b"OUTPUT ON")
---
+++ README.md
@@ -0,0 +1,2 @@
+# Toolbox for Experiments
+In this repo, I will organize some toolbox programs for our experiments.
